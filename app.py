2fbaae388d82efd8efa646f8954db23bf8cf17f0
adnankahfibara <adnanxkb@gmail.com>
2026-01-04 23:39:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask!"
+    return "Aplikasi Flask berhasil dijalankan di Railway!"
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
